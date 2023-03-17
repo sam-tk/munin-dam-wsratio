5f95e3cf32e427a56730ddb1501e72410d669680
Masahiro Yoshitake <sam-tk@x-day.org>
2023-03-17 23:45:28 +0900

    jqからjmespathに変更

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ extern crate simple_munin_plugin;
 mod wsr {
     use simple_munin_plugin::MuninNodePlugin;
     use ureq;
-    use jq_rs;
-    use serde_json;
+    use jmespath::{self, Variable};
+    use strip_bom::*;
 
     const FIELD: &'static str = "wsratio";
 
@@ -15,16 +15,39 @@ mod wsr {
             WsratioPlugin
         }
 
-        pub fn get_wsr() -> f32 {
-            let dam_json_url = "https://www.ktr.mlit.go.jp/tonedamu/teikyo/realtime2/json/E007010.json";
-        
-            let resp = ureq::get(dam_json_url).call().unwrap();
-            let body = resp.into_string().unwrap();
-            let res = jq_rs::run(".damDataList[-1].dataList[0].waterRate", &body).unwrap();
-            let wsr: f32 = serde_json::from_str(&res).unwrap();
-            return wsr
+        pub fn get_wsr() -> f64 {
+            return Self::get_vol() / Self::get_max() * 100.0 
 
         }
+
+        fn get_max() -> f64 {
+            let json_url = "https://www.ktr.mlit.go.jp/tonedamu/teikyo/realtime2/json/E015010.json";
+            let resp = ureq::get(json_url).call().unwrap();
+            let expr = jmespath::compile("waterCapacityList.dataList.*.to_number(validWaterCapacity) | max(@)").unwrap();
+            let body_str = resp.into_string().unwrap();
+            let body_str :&str = body_str.strip_bom();
+            let data = jmespath::Variable::from_json(&body_str).unwrap();
+            let result = expr.search(data).unwrap();
+            result.as_number().unwrap()
+        }
+        
+        fn get_vol() -> f64 {
+            let json_url = "https://www.ktr.mlit.go.jp/tonedamu/teikyo/realtime2/json/E007010.json";
+            let resp = ureq::get(json_url).call().unwrap();
+            let expr = jmespath::compile("damDataList[?observationName!='５ダム' && observationName!='９ダム'].dataList[0].waterCapacity").unwrap();
+            let body_str = resp.into_string().unwrap();
+            let body_str :&str = body_str.strip_bom();
+            let data = jmespath::Variable::from_json(&body_str).unwrap();
+            let result = expr.search(data).unwrap();
+            let variable: &Variable = &*result;
+            let mut total = 0.0;
+            for i in variable.as_array().unwrap() {
+                let v = i.to_string().trim_matches('"').to_string().replace(",","").parse::<f64>().unwrap_or(0.0);
+                total = total + v ;
+            }
+            total
+        }
+        
     }
 
     impl MuninNodePlugin for WsratioPlugin {
